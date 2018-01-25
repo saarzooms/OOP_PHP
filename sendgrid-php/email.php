e056d497d5bd77e6b2485fe4671e4d04e0b227bd
Shamsaagazarzoo M Shaikh <arzoo.rkcet@gmail.com>
2018-01-25 16:03:25 +0530

    Add files via upload

---
+++ sendgrid-php/email.php
@@ -0,0 +1,21 @@
+<?php
+// If you are using Composer
+require 'sendgrid-php.php';
+
+// If you are not using Composer (recommended)
+// require("path/to/sendgrid-php/sendgrid-php.php");
+
+$from = new SendGrid\Email(null, "vishalpatel16@gmail.com");
+$subject = "Hello World from the SendGrid PHP Library!";
+$to = new SendGrid\Email(null, "vishal.rkcet@gmail.com");
+$content = new SendGrid\Content("text/plain", "Hello, Email!");
+$mail = new SendGrid\Mail($from, $subject, $to, $content);
+
+$apiKey = getenv('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');
+$sg = new \SendGrid($apiKey);
+
+$response = $sg->client->mail()->send()->post($mail);
+echo $response->statusCode();
+echo $response->headers();
+echo $response->body();
+?>
